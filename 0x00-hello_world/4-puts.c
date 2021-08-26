4e8f29c9a765da1f5669e119914063a5b6dbff2f
TMH46 <tsegazeabtmh@gmail.com>
2021-08-26 07:15:38 -0700

    Add a file with c program prints using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 int main(void)
 {
+/* we use puts function to print the string to the output stdout stream*/
 puts("\"Programming is like building a multilingual puzzle");
 return(0);
 }
