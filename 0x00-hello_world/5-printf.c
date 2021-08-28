d6f2faf46961da6e442a8bfd879edf19b99890a3
TMH46 <tsegazeabtmh@gmail.com>
2021-08-28 03:29:03 -0700

    Add 5-printf.c file a code that prints using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: return 0 that returns to the function
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
