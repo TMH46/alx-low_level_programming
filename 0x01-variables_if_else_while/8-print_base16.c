dc76bc228dca32923684ab002235d04297de976b
TMH46 <tsegazeabtmh@gmail.com>
2021-08-28 05:46:48 -0700

    Add file with a code prints numbers of base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+  
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: return 0 successful excution
+ *
+ *Description: This code prints numbers of hexadecimal 
+ */
+
+int main(void)
+{
+	char hex;
+
+	for (hex = 0; hex <= 9; hex++)
+	putchar(hex + '0');
+	for (hex = 'a'; hex <= 'f'; hex++)
+	putchar(hex);
+	putchar('\n');
+
+	return (0);
+}
