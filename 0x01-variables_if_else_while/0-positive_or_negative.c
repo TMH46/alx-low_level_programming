d599e066a3c96874aa4992b82d25ad9b98fa3bcf
TMH46 <tsegazeabtmh@gmail.com>
2021-08-28 04:30:46 -0700

    Add file with a code identify random numbers

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,24 +6,24 @@
  *
  * Return: return 0 that returns to the function
  *
- * description: this code identifies the random numbers as positive, negative and zero
+ * Descriptions: Identify the random numbers as positive, negative and zero
  */
 int main(void)
 {
 	int n;
-
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	
-	if(n>0)
+	n = rand() - RAND_MAX / 2;	
+	if (n > 0)
 	{
-		printf("%d is positive\n", n);
-	} else if(n=0)
+	printf("%d is positive\n", n);
+	}
+	else if(n < 0)
 	{
-		printf("%d is zero\n", n);
-	} else
+	printf("%d is negative\n", n);
+	}
+	else
 	{ 
-		printf("%d is negative\n", n);
+	printf("%d is zero\n", n);
 	}
 	return (0);
 }
