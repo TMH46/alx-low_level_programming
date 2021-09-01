e09eb2e6c92ee500501a4b5b58aac59fea0cc98d
TMH46 <tsegazeabtmh@gmail.com>
2021-09-01 04:22:49 -0700

    Add a code prints holberton using _putchar

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -6,12 +6,15 @@
  */
 int main(void)
 {
-	char *c = "Holberton";
-	int i;
-	for (i = 0; i< 10; i++) 
-	{
-		_putchar(c[i]);
-	}
+	_putchar('H');
+	_putchar('O');
+        _putchar('L');
+	_putchar('B');
+	_putchar('E');
+	_putchar('R');
+	_putchar('T');
+	_putchar('O');
+	_putchar('N');
 	_putchar('\n');
 
 	return (0);
