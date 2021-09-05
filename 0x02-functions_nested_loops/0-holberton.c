e9d09db10565e3f22386c5e426fe0ff28a05c56a
TMH46 <tsegazeabtmh@gmail.com>
2021-09-05 07:40:36 -0700

    Add code that checks lowercase & prototype in the header

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *main - Entry point
+ *
+ *Return: return (0) success
+ */
+
+int main(void)
+{
+	_putchar('H');
+	_putchar('O');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+
+	return (0);
+}
+
