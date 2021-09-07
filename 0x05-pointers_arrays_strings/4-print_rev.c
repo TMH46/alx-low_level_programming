d0bac8ecd71c6585489cc1982dc876ff8ec72d65
TMH46 <tsegazeabtmh@gmail.com>
2021-09-07 03:58:05 -0700

    Add code that print string in reverse order

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *print_rev - Prints a string in reverse order
+ *@s: input string
+ */
+
+void print_rev(char *s)
+{
+	int i, j, length;
+
+	i = 0;
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+
+	length = i;
+	for (j = length - 1; j >= 0; j--)
+	{
+		_putchar(*(s + j));
+	}
+
+	_putchar('\n');
+}
